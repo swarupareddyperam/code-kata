5687a148cfb4f37e14dce4ebdb8791bd25be6a7f
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-02 16:14:36 +0530

    CountSpacesinstring.java

---
+++ CountSpacesinstring.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CountSpacesinstring
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+				Scanner sc = new Scanner(System.in);
+        String str= sc.nextLine();
+        int space=0;
+		    char ch;
+		    for(int i=0;i<str.length();i++)
+		    {
+			  ch=str.charAt(i);
+
+			if(ch==' ')
+			{
+		              space++;
+			}
+			
+		}
+		System.out.println(space);
+
+	}
+}
