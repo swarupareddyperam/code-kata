b3e549dc8fb2de9b5700ca6dafdc24f4be9be1e6
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-02 14:26:03 +0530

    Update Min.c

--- Min.c
+++ Min.c
@@ -1,19 +1,23 @@
-
 #include <stdio.h>
 #include<string.h>
+//#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
 
 int main()
 {
+// clrscr();   
 int T1,T2,T3,min1,min2,min3,hr1,hr2,hr3;
-printf("enter the first time in hr and min");
+//printf("enter the first time in hr and min");
 scanf("%d%d",&hr1,&min1);
-printf("enter the second time in hr and min");
+//printf("enter the second time in hr and min");
 scanf("%d%d",&hr2,&min2);
 T1=(hr1*60)+min1;
 T2=(hr2*60)+min2;
 T3=T1-T2;
 hr3=T3/60;
 min3=T3%60;
-printf("difference between two times are  %d:%d",abs(hr3),abs(min3));
+printf("%d %d",abs(hr3),abs(min3));
+//getch();
     return 0;
 }
