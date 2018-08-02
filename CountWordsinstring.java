741b34cfdc6cead40a6b28099a9d8ef749b36754
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-02 15:33:56 +0530

    CountWordsinstring.java

---
+++ CountWordsinstring.java
@@ -0,0 +1,35 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CountWordsinstring
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		//Scanner sc=new Scanner()
+		Scanner sc = new Scanner(System.in);
+		int count=1;
+
+                        //System.out.println("Please enter a string");
+
+                         String ch = sc.nextLine();
+
+                          //System.out.println(ch);
+
+		//int letter=0;
+		for(int i=0;i<ch.length();i++)
+		{
+			if(ch.charAt(i)==' ')
+			{
+			count++ ;
+			}
+			//sum++;
+		}
+		System.out.println(count);
+
+	}
+}
