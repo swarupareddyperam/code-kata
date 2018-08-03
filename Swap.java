bc1fd71c7c8fd67a768630a9b933f189021ee730
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-03 14:52:20 +0530

    Swap.java

---
+++ Swap.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Swap
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		//swap(a,b);
+		int temp;
+		temp=a;
+		a=b;A
+		b=temp;
+		System.out.print(a+" "+b);
+
+	}
+}
