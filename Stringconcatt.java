c689b457e9fda8964dd1f9ae93daee6d9abbda48
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-06 16:08:13 +0530

    Create Stringconcatt.java

---
+++ Stringconcatt.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Stringconcatt
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String name1=sc.next();
+		String name2=sc.next();
+	  System.out.println(name1+name2);
+	}
+}
