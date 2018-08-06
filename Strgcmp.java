556803ea0f282943de4788f5232e68addc4d1f86
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-06 15:52:58 +0530

    Create Strgcmp.java

---
+++ Strgcmp.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Strgcmp
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String name1=sc.next();
+		String name2=sc.next();
+	            int n1=name1.length();
+	            int n2=name2.length();
+		if(n1>=n2)
+		    System.out.println(name1);
+	//	else if(name1==name2)
+			//System.out.println(name1);
+			else 
+			System.out.println(name2);
+	    		
+	    //else
+	        //continue;
+		
+		
+	}
+}
