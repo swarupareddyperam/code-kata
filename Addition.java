f908328e274c5523efa63b7ccacb8b6bef14ae4c
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-09 11:27:10 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Addition
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println(c=a+b);
+	}
+}
