3ba373d5f790d1ca930cf5770bf42fabbe49df03
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-09 10:31:01 +0530

    Update and rename Fibonacc.java to Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -5,11 +5,10 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Fibonacc
+class Fibonacci
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int t1=1,t2=1;
